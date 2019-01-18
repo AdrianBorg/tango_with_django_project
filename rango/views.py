c31d83223d49c3d9a2f046086c3a7964bc0be17d
Adrian Borg <borgadrian@yahoo.co.uk>
2019-01-18 22:47:48 +0000

    Squashed commit of the following:
    commit ecfd151bf7cd12c3052ef3515d92568226a18cfe
    Author: Adrian Borg <borgadrian@yahoo.co.uk>
    Date:   Fri Jan 18 19:50:44 2019 +0000
    
        Completed chapter 4, fixed media url bug, created about template, hosting media file
    
    commit 0a2fee4be1f685cf67236698896c4236999344f9
    Author: Adrian Borg <borgadrian@yahoo.co.uk>
    Date:   Fri Jan 18 18:05:33 2019 +0000
    
        setting up media serving from ch4
    
    commit 873b469dfadebeaec532b6349beb75d46a5e6a99
    Author: Adrian Borg <borgadrian@yahoo.co.uk>
    Date:   Fri Jan 18 17:52:07 2019 +0000
    
        done static part of ch4
    
    commit 70c441b261254526c664a887a30bd200b6e987c7
    Author: Adrian Borg <borgadrian@yahoo.co.uk>
    Date:   Fri Jan 18 17:04:13 2019 +0000
    
        added index template
    
    commit 97c0c38fd05ad7be5822ed0015fcfe9f00698adf
    Author: Adrian Borg <borgadrian@yahoo.co.uk>
    Date:   Fri Jan 18 17:01:10 2019 +0000
    
        update to gitignore

--- rango/views.py
+++ rango/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from tango_with_django_project.settings import MEDIA_URL
+#import os
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <br/>" +
-                        "<a href='/rango/about/'>About</a>")
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the tamplate!
+    context_dict = {'boldmessage': "Crunchy, creamy cookie, candy, cupcake!"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page <br/>" +
-                        "<a href='/rango/'>Index<a/>")
+    return render(request, 'rango/about.html')
